5dc54080e76020053c7facf75050caff9f9ffa53
vitakolosova <kolosova.dev@gmail.com>
2025-03-30 22:47:38 +0200

    css update and blur for input in wishlist items

--- static/wishlist-items.js
+++ static/wishlist-items.js
@@ -2,6 +2,10 @@ function initWishListItems() {
     console.log('hello')
     const items = document.querySelectorAll('[id^="item-"]');
 
+    const setCursorToEnd = (input) => {
+        const length = input.value.length;
+        input.setSelectionRange(length, length);
+    }
     for (const item of items){
         const title = item.querySelector('[id^="title-container-"]')
         const description = item.querySelector('[id^="description-container-"]')
@@ -9,38 +13,44 @@ function initWishListItems() {
         const price = item.querySelector('[id^="price-container-"]')
 
         if (title) {
-        title.addEventListener('click', (e) => {
             const input = title.querySelector('input');
             const titleText = title.querySelector('[id^="title-text-"]');
-            input.hidden = false
-            titleText.hidden = true
-
-        })
-        title.addEventListener('keypress', (e) => {
-            if (e.key === 'Enter') {
-                const input = title.querySelector('input');
-                const titleText = title.querySelector('[id^="title-text-"]');
-                input.hidden = true
-                titleText.hidden = false
-                // Submit the form when 'Enter' is pressed
-                const form = title.closest('form');  // Find the closest form
-                form.submit();  // Submit the form
-            }
-        })
+            title.addEventListener('click', (e) => {
+                input.hidden = false;
+                titleText.hidden = true;
+                input.focus();
+                setCursorToEnd(input);
+                })
+            input.addEventListener('blur', () => {
+                input.hidden = true;
+                titleText.hidden = false;
+            });
+            title.addEventListener('keypress', (e) => {
+                if (e.key === 'Enter') {
+                    input.hidden = true
+                    titleText.hidden = false
+                    // Submit the form when 'Enter' is pressed
+                    const form = title.closest('form');  // Find the closest form
+                    form.submit();  // Submit the form
+                }
+            })
         }
 
         if (description) {
+            const input = description.querySelector('input');
+            const descText = description.querySelector('[id^="description-text-"]');
             description.addEventListener('click', (e) => {
-                const input = description.querySelector('input');
-                const descText = description.querySelector('[id^="description-text-"]');
                 input.hidden = false
                 descText.hidden = true
-
+                input.focus();
+                setCursorToEnd(input);
             })
+            input.addEventListener('blur', () => {
+                input.hidden = true;
+                descText.hidden = false;
+            });
             description.addEventListener('keypress', (e) => {
                 if (e.key === 'Enter') {
-                    const input = description.querySelector('input');
-                    const descText = description.querySelector('[id^="description-text-"]');
                     input.hidden = true
                     descText.hidden = false
                     // Submit the form when 'Enter' is pressed
@@ -50,16 +60,20 @@ function initWishListItems() {
             })
         }
         if (url) {
+            const input = url.querySelector('input');
+            const urlLink = url.querySelector('[id^="url-link-"]');
             url.addEventListener('click', (e) => {
-                const input = url.querySelector('input');
-                const urlLink = url.querySelector('[id^="url-link-"]');
                 input.hidden = false
                 urlLink.hidden = true
+                input.focus();
+                setCursorToEnd(input);
             })
+            input.addEventListener('blur', () => {
+                input.hidden = true;
+                urlLink.hidden = false;
+            });
             url.addEventListener('keypress', (e) => {
                 if (e.key === 'Enter') {
-                    const input = url.querySelector('input');
-                    const urlLink = url.querySelector('[id^="url-link-"]');
                     input.hidden = true
                     urlLink.hidden = false
                     // Submit the form when 'Enter' is pressed
@@ -69,16 +83,20 @@ function initWishListItems() {
             })
         }
         if (price) {
+            const input = price.querySelector('input');
+            const priceValue = price.querySelector('[id^="price-value-"]');
             price.addEventListener('click', (e) => {
-                const input = price.querySelector('input');
-                const priceValue = price.querySelector('[id^="price-value-"]');
                 input.hidden = false
                 priceValue.hidden = true
+                input.focus();
+                setCursorToEnd(input);
             })
+            input.addEventListener('blur', () => {
+                input.hidden = true;
+                priceValue.hidden = false;
+            });
             price.addEventListener('keypress', (e) => {
                 if (e.key === 'Enter') {
-                    const input = price.querySelector('input');
-                    const priceValue = price.querySelector('[id^="price-value-"]');
                     input.hidden = true
                     priceValue.hidden = false
                     const form = price.closest('form');
