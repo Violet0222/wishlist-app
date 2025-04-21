76d014e2db55242c0a933f6d6ac30ce2b64252c3
vitakolosova <kolosova.dev@gmail.com>
2025-04-21 18:51:45 +0200

    update menu_dropdown.js

--- static/modal_window.js
+++ static/modal_window.js
@@ -3,34 +3,46 @@ function modalWindow() {
 
 
 
-    const openModalBtn = document.querySelectorAll(".openModalBtn");
-    const modal = document.querySelectorAll(".modal");
-    const closeModalBtn = document.querySelectorAll(".closeModalBtn");
+    const openModalBtns = document.querySelectorAll(".openModalBtn");
+    const modals = document.querySelectorAll(".modal");
+    const closeModalBtns = document.querySelectorAll(".closeModalBtn");
 
-    // modal window opening
-   openModalBtn.addEventListener("click", () => {
-       modal.style.display = "block";
-       setTimeout(function() {
-           modal.classList.add("open");
-       }, 10);
-   })
-    // Close modal window
-    closeModalBtn.addEventListener("click", function() {
-        modal.classList.remove("open");
-        setTimeout(function() {
-            modal.style.display = "none";
-        }, 300);
-    });
+    openModalBtns.forEach((btn, index) => {
+        const modal = modals[index]
+        const closeModalBtn = closeModalBtns[index]
 
-    // Close modal if clicked outside of modal content
-    window.addEventListener("click", function(event) {
-        if (event.target === modal) {
+        if (!modal || !closeModalBtn) return;
+
+        // modal window opening
+        btn.addEventListener("click", () => {
+            modal.style.display = "block";
+            setTimeout(function() {
+                modal.classList.add("open");
+            }, 10);
+        });
+        // Close modal window
+        closeModalBtn.addEventListener("click", function() {
             modal.classList.remove("open");
             setTimeout(function() {
                 modal.style.display = "none";
             }, 300);
-        }
-    });
+        })
+
+        // Close modal if clicked outside of modal content
+        window.addEventListener("click", function(event) {
+            if (event.target === modal) {
+                modal.classList.remove("open");
+                setTimeout(function() {
+                    modal.style.display = "none";
+                }, 300);
+            }
+        });
+    })
+
+
+
+
+
 
 }
 
